2338a66772f567adbab028db352fe593518ecbdf
odeozome <78852587+Divine-Blessing@users.noreply.github.com>
2022-06-25 09:58:05 +0100

    Add files via upload
    This is my Zuri javascript task

---
+++ app.js
@@ -0,0 +1,7 @@
+ let myName = "My name is Divine-Blessing Oviekhaye";
+ let myHeight  = "My height is 5ft5";
+ let myCountry = "I'm from Nigeria";
+
+ console.log(myName);
+ console.log(myHeight);
+ console.log(myCountry);
